b1bbcd19037fa89d72d75475f3ff02c709c821b8
Thomas Zermeno <madcarrots@gmail.com>
2026-05-09 19:21:53 -0500

    fix: add api url to whitelist part 2 electric...

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,7 +3,8 @@ const whitelist = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'https://dazzling-snickerdoodle-777101.netlify.app/', 
-    'https://inf653-final-statesapi.onrender.com'
+    'https://inf653-final-statesapi.onrender.com', 
+    'https://serene-roentgen-345cd7.netlify.app'
 ];
 
 const corsOptions = {
